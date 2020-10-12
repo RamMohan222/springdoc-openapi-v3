9705e050aefec97c1bebc4ee22f82d8458beaa9c
RamMohan <ram222mohan@gmail.com>
2020-10-13 01:18:04 +0530

    Documentation Annotations are added

--- src/main/java/com/springdoc/openapi/controller/EmployeeController.java
+++ src/main/java/com/springdoc/openapi/controller/EmployeeController.java
@@ -17,6 +17,12 @@ import org.springframework.web.bind.annotation.RestController;
 import com.springdoc.openapi.model.CommonModel;
 import com.springdoc.openapi.model.Employee;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+
 @RestController
 @RequestMapping(path = "/employees", produces = MediaType.APPLICATION_JSON_VALUE)
 public class EmployeeController {
@@ -33,6 +39,12 @@ public class EmployeeController {
 		return ResponseEntity.ok(employees);
 	}
 
+	@Operation(summary = "Get The Employee Details")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Found the employee", content = {
+					@Content(mediaType = "application/json", schema = @Schema(implementation = Employee.class)) }),
+			@ApiResponse(responseCode = "400", description = "Invalid Employee Id", content = @Content),
+			@ApiResponse(responseCode = "404", description = "Employee not found", content = @Content) })
 	@GetMapping("/{employeeId}")
 	public ResponseEntity<Employee> get(@PathVariable String employeeId) {
 		Employee employee = new Employee();

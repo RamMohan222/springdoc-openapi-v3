d92edbd51b8c76f1999434d6f3fb4f01ec5ad45f
RamMohan <ram222mohan@gmail.com>
2021-08-18 20:36:52 +0530

    Create OpenApiConfigWithSecurity.java

---
+++ src/main/java/com/springdoc/openapi/config/OpenApiConfigWithSecurity.java
@@ -0,0 +1,37 @@
+ private static final String SECURITY_SCHEME_NAME = "Bearer oAuth Token";
+
+    /**
+     * Open API Configuration Bean
+     *
+     * @param title
+     * @param version
+     * @param description
+     * @return
+     */
+    @Bean
+    public OpenAPI openApiConfiguration(
+            @Value("${openapi.title}") final String title,
+            @Value("${openapi.version}") final String version,
+            @Value("${openapi.description}") final String description
+    ) {
+        return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement().addList(SECURITY_SCHEME_NAME))
+                .components(
+                        new Components()
+                                .addSecuritySchemes(SECURITY_SCHEME_NAME,
+                                        new SecurityScheme()
+                                                .name(SECURITY_SCHEME_NAME)
+                                                .type(SecurityScheme.Type.HTTP)
+                                                .scheme("bearer")
+                                                .bearerFormat("JWT")
+                                )
+                )
+                .info(new Info()
+                        .title(title)
+                        .version(version)
+                        .description(description)
+                        .termsOfService("Terms of service")
+                        .license(getLicense())
+                        .contact(getContact())
+                );
+    }
